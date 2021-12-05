8f6ddd0e664fb474d9d5963c4d43a79f442f9386
Ebuka Nwabueze <ebukanwabueze@gmail.com>
2021-12-05 23:18:53 +0100

    fetch data function defined

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData =  async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params: {
+            apikey: '2ac4a8c1',
+            s: searchTerm
+        }
+    });
+    console.log(response.data)
+}
+
+fetchData('avengers')
